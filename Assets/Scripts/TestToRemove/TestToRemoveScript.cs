f90325d16a66a206fb0eceb01952f33ad7a042ab
KirillLaptop <Kirill_Yakubov@outlook.com>
2024-08-07 19:08:54 -0400

    My Commit from a different PC

---
+++ Assets/Scripts/TestToRemove/TestToRemoveScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Barista
+{
+    public class TestToRemoveScript : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            print("test");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
